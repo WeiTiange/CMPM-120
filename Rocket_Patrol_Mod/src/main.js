b787d5373967b1ed7d2030b5dbe2223a0545bc70
TIANGE WEI <twei14@ucsc.edu>
2022-04-19 19:12:40 -0700

    Update main.js

--- Rocket_Patrol_Mod/src/main.js
+++ Rocket_Patrol_Mod/src/main.js
@@ -1,3 +1,20 @@
+// Rocekt Patrol Mod create by Tiange Wei, 4/19/2022.
+// It took me about 12 house to complete the project.
+
+// Points breakdown:
+// I created a new spaceship type (with new artwork) that's smaller, moves faster and is worth more points(the green fish)(20).
+// I implemented a simultaneous two player mode. Two players can play at the same time using different key controls, Left Arrow and Right Arrow to move and Up Arrow to fire for player1, A and D to move and Spacebar to fire for player2(30).
+// I redesigned the game's art work, UI, and sound to change theme of the game to a ocean fishing game(60).
+// I also added a loop background music that plays both in Menu and in Play scene.
+
+// Work Cited:
+// https://www.freepik.com/free-vector/ocean-sea-underwater-background-empty-bottom_7058927.htm#query=underwater%20cartoon&position=0&from_view=search
+// https://wenrexa.itch.io/cartoon-fishes
+// https://www.subpng.com/png-tscq0s/download.html
+// https://www.subpng.com/png-q3qcnb/
+// https://mixkit.co
+
+
 let config = {
     type: Phaser.CANVAS,
     width: 640,
